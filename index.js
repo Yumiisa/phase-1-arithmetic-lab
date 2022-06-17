7adb91d58b0083599dd84c14546671d0084802be
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-06-17 10:02:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 10) + 1;
+const num3 = 85;
+const num4 = 9;
+const mod = num3 % num4;
+const max = Math.max( 5, 1, 12,17, 20 );
